daff7801844090f08d1b3be3c82a65fccc6134a9
firegoboom <miketheklein@gmail.com>
2017-01-09 17:23:02 -0800

    what

---
+++ src/hash.js
@@ -0,0 +1,23 @@
+// export default class HashTable {
+//   constructor() {
+//     this.elements = {}
+//   }
+//
+//   put(key, value) {
+//     this.elements[key] = value
+//   }
+//
+//   get(key) {
+//
+//   }
+//
+//   contains() {
+//     return true !== key
+//   }
+//
+//
+//
+//
+
+
+}
